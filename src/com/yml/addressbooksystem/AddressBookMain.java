b7032bf99cc6e7e730ffe12464027c17db64dc16
Sujay S Shenoy <sujay.shenoy@ymedialabs.com>
2021-09-02 19:43:23 +0530

    private main changed to public

--- src/com/yml/addressbooksystem/AddressBookMain.java
+++ src/com/yml/addressbooksystem/AddressBookMain.java
@@ -1,9 +1,9 @@
 import java.io.PrintWriter;
 
 class AddressBookMain{
-    private static void main(String args[]) {
+    public static void main(String args[]) {
         PrintWriter out = new PrintWriter(System.out,true);
-        
+
         out.println("Welcome to Address Book Program");
 
         out.close();
