1448c8b7ada194c561fa48bf2592aad5a5c5fa5e
Sujay S Shenoy <sujay.shenoy@ymedialabs.com>
2021-09-02 19:41:33 +0530

    Welcome message added

---
+++ src/com/yml/addressbooksystem/AddressBookMain.java
@@ -0,0 +1,12 @@
+import java.io.PrintWriter;
+
+class AddressBookMain{
+    private static void main(String args[]) {
+        PrintWriter out = new PrintWriter(System.out,true);
+        
+        out.println("Welcome to Address Book Program");
+
+        out.close();
+    }
+
+}
